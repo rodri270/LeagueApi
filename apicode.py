7cdb4b1088fd4483472bf47d9111b4f7a2bb6137
Alexander Rodriguez <30503623+rodri270@users.noreply.github.com>
2020-10-09 14:24:41 -0400

    TEsting with a user that is ranked

--- apicode.py
+++ apicode.py
@@ -5,7 +5,7 @@ api_key = 'RGAPI-e9e4857b-edc4-491c-88bb-9fe2bd27c34d'
 watcher = LolWatcher(api_key)
 my_region = 'na1'
 
-me = watcher.summoner.by_name(my_region, 'Hay1tsme')
+me = watcher.summoner.by_name(my_region, 'The Midget Cow')
 print(me)
 
 my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
--- homebrewapiTest.py
+++ homebrewapiTest.py
@@ -14,14 +14,4 @@ apikey ='RGAPI-e9e4857b-edc4-491c-88bb-9fe2bd27c34d'
 #get request for pulling the general user info 
 UserRequest = requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}".format(SummonerName, apikey))
 
-#assigned the request to a value in text
-TextValue = UserRequest.text
-
-#convert value to string (not sure if this is needed)
-str(TextValue)
-
-#print string but only characters between 215 and 230 
-print(TextValue[215:230])
-
-#next step is to figure out a way to have it automaticly pull the info needed without having to manually tell it where the text is 
-#OR figure out how to get the request to show up in json and then parse the info that way 
+print (UserRequest.json)
