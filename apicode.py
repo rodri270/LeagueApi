2eeff220215a0d0024aaa13935e6c4379e4444a6
Alexander Rodriguez <30503623+rodri270@users.noreply.github.com>
2020-10-08 20:01:42 -0400

    this is my first commit with code i found online

--- apicode.py
+++ apicode.py
@@ -1,1 +1,12 @@
-print("hello world")
+from riotwatcher import LolWatcher, ApiError
+
+# golbal variables
+api_key = 'RGAPI-e9e4857b-edc4-491c-88bb-9fe2bd27c34d'
+watcher = LolWatcher(api_key)
+my_region = 'na1'
+
+me = watcher.summoner.by_name(my_region, 'MortemDeCaelo')
+print(me)
+
+my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
+print(my_ranked_stats)
