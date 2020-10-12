84ce1963b23cfbdfb6b20795e5c5b46a82711400
Alexander Rodriguez <30503623+rodri270@users.noreply.github.com>
2020-10-12 14:53:14 -0400

    :poggers:
    Was able to pull information out of the json and pipe it into a new request

--- homebrewapi.py
+++ homebrewapi.py
@@ -1,28 +1,40 @@
 ## this will use all the get commands offered by the riot docs 
 ## pip instal requests
+## pip instal pandas 
 ## api = RGAPI-e9e4857b-edc4-491c-88bb-9fe2bd27c34d
 ## install requests library 
 
 import requests
 import json
 
+
 #request input for who they wanna search up 
 SummonerName = input("Please enter summoner name: ")
 
 #varioable for the api key
-apikey ='RGAPI-e9e4857b-edc4-491c-88bb-9fe2bd27c34d'
+apikey ='RGAPI-5de70cb1-7458-4726-b4ba-3f3480c23160'
 
 #get request for pulling the general user info 
 UserRequest = requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}".format(SummonerName, apikey))
 
-#assigned the request to a value in text
-TextValue = UserRequest.text
 
-#convert value to string (not sure if this is needed)
-str(TextValue)
+#this will print in just raw text 
+#print (UserRequest.text)
 
-#print string but only characters between 215 and 230 
-print(TextValue[215:230])
+#this sets the resquest output as text and to the json_object_string
+json_object_string = UserRequest.text
+
+#this lets you take the output from the top variable and load the data into a json dictionary 
+json_object = json.loads(json_object_string)
+
+#this lets you call the value from above, pick a certain value and assign it to a new variable 
+Summonerid = (json_object["id"])
+
+#New get request for the ranked aspect of the user 
+UserRankedRequest = requests.get("https://na1.api.riotgames.com//lol/league/v4/entries/by-summoner/{}?api_key={}".format(Summonerid, apikey))
+
+#prints everything in a raw string
+print (UserRankedRequest.text)
 
 #next step is to figure out a way to have it automatically  pull the info needed without having to manually tell it where the text is 
 #OR figure out how to get the request to show up in json and then parse the info that way 
